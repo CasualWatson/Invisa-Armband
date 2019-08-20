43dad1c8a21e46e87310a42115ff6c334f0f3123
CasualWatson <i3watson@tuta.io>
2019-08-19 23:55:56 -0500

    Setting up jank

---
+++ Invisa-Armband/Invisa-Armband/BandPatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Harmony;
+
+namespace Invisa_Armband
+{
+    public class Harmony_Patch
+    {
+        public Harmony_Patch()
+        {
+            try
+            {
+                HarmonyInstance hInstance = HarmonyInstance.Create("Lobotomy.Watson & NEET.Invisa-Armband");
+                hInstance.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception excep)
+            {
+                FileLog.Log("Invisa_Armband Patching Threw an Exception... " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt"));
+                FileLog.Log(excep.ToString());
+                FileLog.Log(excep.Message);
+                FileLog.Log(excep.TargetSite.Name);
+            }
+        }
+    }
+}
